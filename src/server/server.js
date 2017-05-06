ab6786ed8f59a43dc97f054ba06093f106e11454
horngyih <horngyih@yahoo.com>
2017-05-06 10:53:38 +0800

    - Switched default port to 9897

--- src/server/server.js
+++ src/server/server.js
@@ -14,7 +14,7 @@ var server = _express();
 server.use( _bodyParser.json() );
 
 var rootPath = process.env.SERVER_ROOT || "";
-var serverPort = process.env.SERVER_PORT || 80;
+var serverPort = process.env.SERVER_PORT || 9897;
 serverPort = ( typeof serverPort === "number" )? serverPort : parseInt( serverPort );
 
 function path( subpath ){
