6e8157b4fa3b0cb76decff90de6e6203f81cca31
horngyih <horngyih@yahoo.com>
2017-05-06 10:33:07 +0800

    [Initial Commit]\n - Added server main script;\n - Added myClearController that handles myClear messages and headers;

---
+++ src/server/server.js
@@ -0,0 +1,43 @@
+var _express = require( 'express' );
+var _bodyParser = require( 'body-parser' );
+
+var myClearController = require( './myClearController' );
+myClearController.use(function printNotification( notification ){
+    var result = "";
+    for( var prop in notification ){
+        result += notification[prop] + "|";
+    }
+    console.log( result );
+});
+
+var server = _express();
+server.use( _bodyParser.json() );
+
+var rootPath = process.env.SERVER_ROOT || "";
+var serverPort = process.env.SERVER_PORT || 9009;
+serverPort = ( typeof serverPort === "number" )? serverPort : parseInt( serverPort );
+
+function path( subpath ){
+    var result = rootPath + "/";
+    if( subpath ){
+        result += subpath + "/";
+    }
+    return result;
+}
+
+server.get( path(), function( req, res ){
+    res.send( "The Droids You're Looking For are not here." );
+});
+
+server.post( path(), function( req, res ){
+    //console.log( new Date() + " : ", req.body );
+    var response =myClearController.handleIncomingMessage( req.body );
+    res.status(200).json( response );
+});
+
+server.listen( serverPort, function(){
+    console.log( "Root Path : " + rootPath );
+    console.log( "Server Port : " + serverPort );
+
+    console.log( "Listening on ... " + serverPort );
+});
