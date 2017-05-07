70ee89cb7a38a84b3fd0cc1cbe6e1c7557e5cba7
horngyih <horngyih@yahoo.com>
2017-05-08 02:28:42 +0800

    - Refactored SFTPUtil Unit Test to get test properties.

--- test/main/java/com/occamsrazor/myclear/TestFilerReader.java
+++ test/main/java/com/occamsrazor/myclear/TestFilerReader.java
@@ -28,8 +28,23 @@ public class TestFilerReader {
 
     @Test
     public void testSFTPUtil() {
+        String userProp = System.getProperty( "sftp.test.user" );
+        String passwordProp = System.getProperty( "Sftp.test.password" );
+        String hostProp = System.getProperty( "sftp.test.host" );
+        String portProp = System.getProperty( "sftp.test.port" );
+
+        String user = ( userProp != null )? userProp : "bnsuser";
+        String password = ( userProp != null )? passwordProp : "password1";
+        String host = ( hostProp != null )? hostProp : "localhost";
+
+        int port = 2222;
+        try{
+            port = Integer.parseInt( ( portProp != null )? portProp : "2222" );
+        } catch( NumberFormatException numEx ){}
+
         JSch.setConfig("StrictHostKeyChecking", "no");
-        String fileContent = SFTPUtil.getFile("bnsuser", "password1", "localhost", 2222, "./bns/1001/1705/BNS-170507-01.dat");
+
+        String fileContent = SFTPUtil.getFile( user, password, host, port, "./bns/1001/1705/BNS-170507-01.dat");
         assertNotNull("Should return a non-null String representation of the File Content", fileContent);
         String[] lines = fileContent.split("\n");
         assertEquals("Should return the expected lines of text", 12, lines.length);
